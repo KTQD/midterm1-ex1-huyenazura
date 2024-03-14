ae4500a946afc33824c83316cd94a38e963b4304
huyenazura <145855494+huyenazura@users.noreply.github.com>
2024-03-14 08:02:25 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,25 @@
-public class Item {
+public abstract class Item {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    protected String ID;
+    float price;
+
+    public Item(String name, String ID, int price) {
+        this.name = name;
+        this.ID = ID;
+        this.price = price;
+    }
+
+    void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+    }
 }
