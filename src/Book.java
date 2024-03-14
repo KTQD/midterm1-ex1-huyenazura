3fc12e0b59fb953c1fe791baff1c5ae5e6645865
huyenazura <145855494+huyenazura@users.noreply.github.com>
2024-03-14 08:02:55 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,13 @@
-public class Book {
+public class Book extends Item{
+    String author;
+    int numberOfPages;
+    String genre;
+
+    public Book(String name, String ID, int price, String author, int numberOfPages, String genre) {
+        super(name, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
 }
+
