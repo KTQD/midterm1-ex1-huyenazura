5d38858f0681b36036a615c9d04459c9c707cb96
huyenazura <145855494+huyenazura@users.noreply.github.com>
2024-03-14 08:01:10 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3=new MP3("ok","a12",12, 12);
         mp3.showInfo();
 
-        Book book;
+        Book book=new Book("ko","a23",2,"tacgia",23,"not bt");
         book.showInfo();
-
     }
 }
